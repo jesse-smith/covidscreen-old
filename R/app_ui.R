bccccdcbb37698c65826574d3e88a97a59839e95
Jesse Smith <jesse.smith@stjude.org>
2021-10-08 16:39:43 -0500

    Update favicon

--- R/app_ui.R
+++ R/app_ui.R
@@ -39,7 +39,7 @@ golem_add_external_resources <- function(){
   )
 
   tags$head(
-    favicon(ext = "png"),
+    favicon(),
     bundle_resources(
       path = app_sys('app/www'),
       app_title = 'covidscreen'
